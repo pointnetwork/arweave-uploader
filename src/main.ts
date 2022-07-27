77b0e38476b3ffe9f8b33483fc5b4b8e68cb1792
Brian Alonso <brian.alonso.a@gmail.com>
2022-07-26 22:20:35 -0300

    deactivate

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
 import config from 'config';
 import axios from 'axios';
-import { arweaveUploaderFactory } from './arweaveUploader';
-import { queueBroker } from './utils/queueBroker';
+// import { arweaveUploaderFactory } from './arweaveUploader';
+// import { queueBroker } from './utils/queueBroker';
 import './healthServer';
 import { log } from './utils/logger';
-import { UPLOAD_TO_ARWEAVE } from './utils/queueNames';
+// import { UPLOAD_TO_ARWEAVE } from './utils/queueNames';
 import { safeStringify } from './utils/safeStringify';
 import { fromMinutesToMilliseconds } from './utils/fromMinutesToMilliseconds';
 
 export async function main() {
   log.info('Subscribing to upload queue');
-  await queueBroker.subscribe(UPLOAD_TO_ARWEAVE, {
-    handlerFactory: arweaveUploaderFactory,
-    maxConcurrency: config.get('arweave_uploader.max_concurrency'),
-  });
+  // await queueBroker.subscribe(UPLOAD_TO_ARWEAVE, {
+  //   handlerFactory: arweaveUploaderFactory,
+  //   maxConcurrency: config.get('arweave_uploader.max_concurrency'),
+  // });
 }
 
 process.on('uncaughtException', function (error: any) {
